51250155ed2771eebbb354aea8e3d985b425c13c
Gary <gp@limestripes.us>
2018-10-12 23:59:10 -0600

    Meal of day showcase display

--- src/components/MealOfDay.js
+++ src/components/MealOfDay.js
@@ -4,6 +4,9 @@ import SecondaryNav from './SecondaryNav'
 import {addToMyRecipes, deleteFromMyRecipes} from '../actions'
 
 class MealOfDay extends Component {
+state = {
+	currentStep: 0,
+}
 
 saveRecipe = (e, recipe) => {
 	e.preventDefault();
@@ -25,12 +28,19 @@ render (){
 	const recipeOnScope = calendar[day][meal];
 	const recipeObject = food[recipeOnScope];
 	const inMyRecipes = recipes.myRecipes.filter((cr) => (cr.id === food[recipeOnScope].id) );
+	const activeStep = this.state.currentStep;
 
 	return (
 		<div className={`meal-ofday ${meal}-container`}>
 			<SecondaryNav title={day + "'s " + meal} routeBack="/" />
-			<div className="container">
-				<h3>{recipeOnScope}</h3>
+			<div className="container" >
+			<div className="row">
+				<div className="col-sm-6 recipe-showcase-left">
+				<h3>{recipeOnScope}</h3> 
+				{ recipeObject.steps && recipeObject.steps.length>0
+					? <div className="step-showcase-counter">{activeStep + 1}</div>
+					: null
+				}
 
 				{ recipeObject.source && inMyRecipes.length === 0
 					? <a href="" onClick={(e) => this.saveRecipe(e, recipeObject)} className="btn btn-primary">+ Save to my recipes</a>
@@ -40,6 +50,30 @@ render (){
 					? <a href="" onClick={(e) => this.removeRecipe(e, recipeObject)} className="btn btn-primary btn-add-recipe">- Remove from my recipes</a>
 					: null
 				}
+
+				{ recipeObject.url
+					? <a href={recipeObject.url} target="_blank">Open original recipe &rarr;</a>
+					: null
+				}
+
+					
+				{ recipeObject.steps && recipeObject.steps.length>0
+					? <div className="mod-steps">
+						{recipeObject.steps.map((step, index) => 
+						<div key={index} className={`mod-step ${index === activeStep ? 'active': ''} ${index === activeStep -1 ? 'pre-active' : ''}`}>
+							{step}
+						</div>
+						)}
+					  </div>
+					: null
+				}
+				
+				</div>
+
+				<div className="col-sm-6 recipe-showcase-right">
+					<div className="recipe-lg-thumb" style={{backgroundImage: `url(${recipeObject.image})`}}></div>
+				</div>
+				</div>
 			</div>
 			
 		</div>
